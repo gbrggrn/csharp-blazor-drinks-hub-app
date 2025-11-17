48dc8fba6f6571f7279bb5a3e6432f49d449e675
gbrggrn <gustberg107@student.kau.se>
2025-11-17 19:59:28 +0100

    Drink model updated

--- DrinksHubAPI/Data/Drink.cs
+++ DrinksHubAPI/Data/Drink.cs
@@ -1,6 +1,26 @@
-﻿namespace DrinksHubAPI.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrinksHubAPI.Data
 {
 	public class Drink
 	{
+		public int id { get; set; }
+
+		[Required]
+		[MaxLength(100)]
+		public string Name { get; set; }
+
+		[Required]
+		[MaxLength(500)]
+		public string Description { get; set; } = string.Empty;
+
+		[Required]
+		[MaxLength(50)]
+		public string Category { get; set; } = string.Empty;
+
+		[Required]
+		[MaxLength(50)]
+		public string Type { get; set; } = string.Empty;
+		public string ImageUrl { get; set; } = string.Empty;
 	}
 }
