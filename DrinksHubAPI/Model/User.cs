92f4c6fc5dc92f642325f7d2df98f5118e81b9c8
gbrggrn <gustberg107@student.kau.se>
2025-12-01 09:16:56 +0100

    Nuget auth packages installed + User model updated

--- DrinksHubAPI/Model/User.cs
+++ DrinksHubAPI/Model/User.cs
@@ -6,13 +6,20 @@ namespace DrinksHubAPI.Model
 	{
 		public int Id { get; set; }
 
+		[Required] 
+		public string Name { get; set; } = string.Empty;
+
+		[Required] 
+		public string Email { get; set; } = string.Empty;
+
+
 		[Required]
 		[MaxLength(50)]
 		public string Username { get; set; } = string.Empty;
 
 		[Required]
 		[MaxLength(50)]
-		public string Password { get; set; } = string.Empty;
-		public string Role { get; set; } = string.Empty;
+		public string PasswordHash { get; set; } = string.Empty;
+		public string Role { get; set; } = "User";
 	}
 }
