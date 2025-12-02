9e19c3c2579aee4a5668a6e7f65b1a3553af029a
gbrggrn <gustberg107@student.kau.se>
2025-12-02 19:08:59 +0100

    Starting to hook up auth service -> API

---
+++ DrinksHubApp/DrinksHubApp/Services/AuthService.cs
@@ -0,0 +1,33 @@
+﻿using DrinksHubApp.DTOs;
+using static System.Net.WebRequestMethods;
+
+namespace DrinksHubApp.Services
+{
+	public class AuthService
+	{
+		HttpClient _http;
+
+		public AuthService(HttpClient http)
+		{
+			_http = http;
+			_http.BaseAddress = new Uri("https://localhost:5119");
+		}
+
+		public async Task<bool> LoginAsync(LoginRequestDTO loginRequestDTO)
+		{
+			if (loginRequestDTO == null)
+			{
+				return false;
+			}
+
+			var response = await _http.PostAsJsonAsync("api/Auth/login", loginRequestDTO);
+
+			if (!response.IsSuccessStatusCode)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
