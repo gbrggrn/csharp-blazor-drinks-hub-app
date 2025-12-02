7463ab878be62c6f6dbdc321d1dd3c92dea49d33
gbrggrn <gustberg107@student.kau.se>
2025-12-02 11:29:19 +0100

    Added admin DB seed

--- DrinksHubAPI/Data/DemoData.cs
+++ DrinksHubAPI/Data/DemoData.cs
@@ -1,11 +1,32 @@
 ﻿
+using DrinksHubAPI.DataAccess;
+using DrinksHubAPI.Model;
+using Microsoft.AspNetCore.Identity;
+using System.Runtime.CompilerServices;
+
 namespace DrinksHubAPI.Data
 {
 	public class DemoData
 	{
-		internal static async Task InitializeAsync(IServiceProvider serviceProvider)
+		private readonly IUserRepository _userRepository;
+		public DemoData(IUserRepository userRepository)
 		{
-			
+			_userRepository = userRepository;
+		}
+
+		internal async Task InitializeAsync(IServiceProvider serviceProvider)
+		{
+			var hasher = new PasswordHasher<User>();
+
+			var seedAdmin = new User
+			{
+				Username = "seedAdmin",
+				Role = "Admin"
+			};
+
+			seedAdmin.PasswordHash = hasher.HashPassword(seedAdmin, "Admin123!");
+
+			await _userRepository.AddAsync(seedAdmin);
 		}
 	}
 }
